6c32c1b59cd415357b99d6f461603f3a69d73043
NishaM2 <nisham2522@gmail.com>
2025-03-15 22:44:09 +0530

    useIsOnline

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,20 +1,24 @@
-//swr is a popular React library that creates a lot of these hooks for you,
-//  and you can use it directly.
-import useSWR from 'swr'
+import { useEffect, useState } from 'react'
 
-// const fetcher = (url) => fetch(url).then((res) => res.json());
-const fetcher = async function(url) {
-  const data = await fetch(url);
-  const json = await data.json();
-  return json;
-};
+function useIsOnline() {
+  const [isOnline, setIsOnline] = useState(window.navigator.onLine);
 
-function Profile() {
-  const { data, error, isLoading } = useSWR('https://dummyjson.com/todos', fetcher)
- 
-  if (error) return <div>failed to load</div>
-  if (isLoading) return <div>loading...</div>
-  return <div>hello, you have {data.todos.length} todos!</div>
+  useEffect(() => {
+    window.addEventListener('online', () => setIsOnline(true));
+    window.addEventListener('offline', () => setIsOnline(false));
+  }, [])
+
+  return isOnline;
 }
 
-export default Profile
+function App() {
+  const isOnline = useIsOnline(5);
+
+  return (
+    <>
+      {isOnline ? "You are online yay!" : "You are not online"}
+    </>
+  )
+}
+
+export default App
