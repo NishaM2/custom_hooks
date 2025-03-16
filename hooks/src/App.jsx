a1324ddf5302eb5b8e9e0b21b1d983b9cda5beda
NishaM2 <nisham2522@gmail.com>
2025-03-16 19:50:43 +0530

    useInterval

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,28 +1,22 @@
-import { useEffect, useState } from 'react'
-
-const useMousePointer = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-
-  const handleMouseMove = (e) => {
-    setPosition({ x: e.clientX, y: e.clientY });
-  };
-
-  useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove);
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
-
-  return position;
-};
+import { useEffect, useState } from 'react';
 
 function App() {
-  const mousePointer = useMousePointer();
+  const [count, setCount] = useState(0);
+  const useInterval = (callback, delay) => {
+    useEffect(() => {
+      const intervalId = setInterval(callback, delay);
+  
+      return () => clearInterval(intervalId);
+    }, [callback, delay]);
+  };
+  
+  useInterval(() => {
+    setCount(c => c + 1);
+  }, 1000)
 
   return (
     <>
-      Your mouse position is {mousePointer.x} {mousePointer.y}
+      Timer is at {count}
     </>
   )
 }
