46564e98e12738c18658c4e7b83453a429819865
NishaM2 <nisham2522@gmail.com>
2025-03-16 20:14:52 +0530

    useDebounce

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,23 +1,34 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+
+function useDebounce(value, timeout) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [value]);
+
+  return debouncedValue;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-  const useInterval = (callback, delay) => {
-    useEffect(() => {
-      const intervalId = setInterval(callback, delay);
-  
-      return () => clearInterval(intervalId);
-    }, [callback, delay]);
-  };
-  
-  useInterval(() => {
-    setCount(c => c + 1);
-  }, 1000)
+  const [value, setValue] = useState(0);
+  const debouncedValue = useDebounce(value, 500);
+
+  useEffect(() => {
+    fetch("")
+  }, [debouncedValue])
 
   return (
-    <>
-      Timer is at {count}
-    </>
+    <div>
+      Debounced value is {debouncedValue}
+      <input type="text" onChange={e => setValue(e.target.value)} />
+    </div>
   )
 }
 
